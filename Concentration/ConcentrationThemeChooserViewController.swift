df4b0d9dea9ac7e5d21b522b663c51574e59e8c7
Gteoharov <Gteoharov@users.noreply.github.com>
2018-05-10 11:56:36 +0300

    Multiple MVC

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -17,14 +17,47 @@ class ConcentrationThemeChooserViewController: UIViewController {
         "Faces" : "😎😃😇🤣😍🤩🤬🤯🤪🤓🧐🤨"
     ]
     
+    // changeTheme method:
+    // 1. How to find theme in splitView for iPad versions and iPhones + models
+    // 2. How to hold something in the heap that gets trown off a navigation stack
+    // 3. How to push things on navigation controller without segue
+    // 4. How to segue from code
+    
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        //1...
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        //2...
+        } else if let cvc = lastSegueToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            //3...
+            navigationController?.pushViewController(cvc, animated: true)
+            
+        } else {
+            //4...
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
     
     // MARK: - Navigation
+    
+    private var lastSegueToConcentrationViewController: ConcentrationViewController?
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
                     cvc.theme = theme
+                    lastSegueToConcentrationViewController = cvc
                 }
             }
         }
